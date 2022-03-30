217b28a7da7380c8be5b5072912785115853afd1
abbas0001 <84105420+abbas0001@users.noreply.github.com>
2022-03-30 22:15:08 +0430

    Update get_panel_expire_date.sh

--- get_panel_expire_date.sh
+++ get_panel_expire_date.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-USERNAME=09029451328
-PASSWORD=Aa741852369
+USERNAME=##HERE##
+PASSWORD=##HERE##
 
 DATA=$(curl -s -d "username=$USERNAME&password=$PASSWORD" https://niksms.com/fa/publicapi/GetPanelExpireDate)
 echo $DATA
